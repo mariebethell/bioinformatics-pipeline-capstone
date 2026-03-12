9f189752f82263256af74038ea9ffeeca93512a6
Marie Bethell <bethe009@csusm.edu>
2026-03-12 13:26:16 -0700

    Add to_string() method, update style

--- graph/graph.py
+++ graph/graph.py
@@ -9,41 +9,54 @@ class StageState(Enum):
     ERROR = 5
 
 class Node:
-    def __init__(self, nodeNum):
-        self.nodeNum = nodeNum
-        self.tool = ""
+    def __init__(self, node_num, tool):
+        self.node_num = node_num
+        self.tool = tool
         self.args = {}
         self.inputs = {}
-        self.ouptuts = {}
-        self.prevNode: Node
-        self.nextNode: Node
+        self.outputs = {}
+        self.prev_node: Node
+        self.next_node: Node
         self.state = StageState.NEW
+
+    def to_string(self):
+        print(f"Node number: {self.node_num}")
+        print(f"Tool: {self.tool}")
+        print(f"Arguments: {self.args}")
+        print(f"Inputs: {self.inputs}")
+        print(f"Outputs: {self.outputs}")
+        if (self.prev_node != None):
+            print(f"Previous node: {self.prev_node.node_num}, {self.prev_node.tool}")
+        if (self.next_node != None):
+            print(f"Next node: {self.next_node.node_num}, {self.next_node.tool}")
+        print(f"Current State: {self.state}")
 
 class Graph:
     def __init__(self):
         self.nodes = {} # Dictionary of node num keys and Nodes. A dict automatically preserves the order of insertion since Python 3.7.
     
-    def getNode(self, nodeNum) -> Node:
-        return self.nodes[nodeNum]
+    def get_node(self, node_num) -> Node:
+        return self.nodes[node_num]
     
-    def getFirstNode(self) -> Node:
+    def get_first_node(self) -> Node:
         return next(iter((self.nodes).values()), None) # Returns first node or None if graph is empty.
 
-    def addNode(self, node, prev, next):
-        node.prevNode = prev
-        node.nextNode = next
+    def add_node(self, node, prev, next):
+        node.prev_node = prev
+        node.next_node = next
 
-        self.nodes[node.nodeNum] = node
+        self.nodes[node.node_num] = node
     
 if __name__ == "__main__":
-    first = Node(1)
-    second = Node(2)
+    first = Node(1, "FastQC")
+    second = Node(2, "Trimmomatic")
 
     graph = Graph()
 
-    graph.addNode(first, None, second)
-    graph.addNode(second, first, None)
+    graph.add_node(first, None, second)
+    graph.add_node(second, first, None)
 
     # Test getting node information
-    print(graph.getFirstNode().nodeNum)
-    print(graph.getNode(2).nodeNum)
+    print(graph.get_first_node().to_string())
+    print()
+    print(graph.get_node(2).to_string())
