6e4b39a84ccf85c0a3749a056f30393c889db6c9
Maxwell Bowman <bowmanfmaxwell@gmail.com>
2026-03-12 23:55:16 -0700

    Created the fastqc entry in the tool widgets list. Created a widget generator for the tool node in its wrapper which will automatically populate the tool node with the required fields for a tool. Also made a rough get_value and set_value for the tool node, unsure if it will be future proof.

--- presentation.py
+++ presentation.py
@@ -1,4 +1,4 @@
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
 from NodeGraphQt import NodeGraph, BaseNode, NodeBaseWidget
 from abc import ABC, abstractmethod
 
@@ -106,7 +106,9 @@ class PipelineWorkbenchVC(PanelController):
         # TODO figure out some way to make it so nodes cannot go off screen
         
         # populating with nodes (one test one for now)
-        node_graph.add_node = node_graph.create_node('bioinformatics_capstone.ToolNode', name='tool')
+        tool = 'fastqc'
+        node_test = node_graph.create_node('bioinformatics_capstone.ToolNode', name=tool)
+        node_test.build_widgets(tool)
 
         gr_widget = node_graph.widget
         
@@ -187,26 +189,154 @@ class SettingsView(QtWidgets.QWidget):
 # set of widgets 
 TOOL_WIDGETS = { # note: QSlider, QCheckBox, QComboBox
     'fastqc' : [
-        {'type': 'slider', 'name': 'thread_slider', 'label': 'Number of Threads', 'default': 1 },
-        {'type': 'checkbox', 'name': 'quiet_check', 'label': 'Quiet', 'default': False },
-        {'type': 'checkbox', 'name': 'nogroup_check', 'label': 'NoGroup', 'default': False },
-        {'type': 'slider', 'name': 'kmers_slider', 'label': 'Kmer Length', 'default': 7 },
-        {'type': 'text_entry', 'name': 'adapters_text_input', 'label': 'Adapters', 'default': None }, # paired with a checkbox below
-        {'type': 'checkbox', 'name': 'adapters_checkbox', 'label': 'Adapters?', 'default': False},
-        {'type': 'text_entry', 'name': 'contaminants_text_input', 'label': 'Contaminants', 'default': None}, # paired with a checkbox below
-        {'type': 'checkbox', 'name': 'contaminants_check', 'label': 'Contaminants?', 'default': False}
+        {'type': 'slider', 'name': 'thread_slider', 'label': 'Number of Threads: 1', 'default': 1, 'need_label': True },
+        {'type': 'checkbox', 'name': 'quiet_check', 'label': 'Quiet', 'default': False, 'need_label': False },
+        {'type': 'checkbox', 'name': 'nogroup_check', 'label': 'NoGroup', 'default': False, 'need_label': False },
+        {'type': 'slider', 'name': 'kmers_slider', 'label': 'Kmer Length: 7', 'default': 7, 'need_label': True },
+        {'type': 'text_entry', 'name': 'adapters_text_input', 'label': 'Adapters', 'default': None, 'need_label': False}, # paired with a checkbox below
+        {'type': 'checkbox', 'name': 'adapters_checkbox', 'label': 'Set Adapters', 'default': False, 'need_label': False},
+        {'type': 'text_entry', 'name': 'contaminants_text_input', 'label': 'Contaminants', 'default': None, 'need_label': False}, # paired with a checkbox below
+        {'type': 'checkbox', 'name': 'contaminants_check', 'label': 'Set Contaminants', 'default': False, 'need_label': False},
+        {'type': 'combo_box', 'name': 'file_format_combobox', 'label': 'File Format', 'default': 'fastq', 'need_label': True}
     ]
 }
 
 # Node responsible for representing a tool within the pipeline & its wrapper
 class ToolNodeWrapper(NodeBaseWidget):
-    pass
+    def __init__(self, tool=None, parent=None):
+        super().__init__(parent)
+
+        self.widgets = {}
+        self.mutable_labels = {}
+
+        container = QtWidgets.QWidget()
+
+        # sets font color for widget labels in the node
+        container.setStyleSheet(
+            """QLabel, QCheckBox{
+            color: white;}
+            """)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0,0,0,0)
+
+        # building widgets dynamically
+        for widget_def in TOOL_WIDGETS[tool]:
+            w_type = widget_def['type']
+            w_label = widget_def['label']
+            w_default = widget_def['default']
+            w_name = widget_def['name']
+            need_label = widget_def['need_label']
+
+            widget = None
+
+            if w_type == 'slider':
+                widget = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+
+                if w_name == 'thread_slider':
+                    widget.setMinimum(1)
+                    widget.setMaximum(128)
+                elif w_name == 'kmers_slider':
+                    widget.setMinimum(1)
+                    widget.setMaximum(20)
+                
+                widget.setValue(w_default)
+
+                # lambda function passes in the current widget name and the value from the signal to update label
+                widget.valueChanged.connect(
+                    lambda value, name=w_name : self.update_label(name, value)
+                    )
+
+            elif w_type == 'checkbox':
+                widget = QtWidgets.QCheckBox(w_label)
+
+            elif w_type == 'text_entry':
+                widget = QtWidgets.QPlainTextEdit()
+                widget.setPlaceholderText(w_label)
+                widget.setReadOnly(True)
+                widget.setMaximumHeight(30)
+
+            elif w_type == 'combo_box':
+                #bam, sam, fastq
+                widget = QtWidgets.QComboBox()
+                widget.addItems(['fastq', 'sam', 'bam'])
+            
+            # if the widget was succesfully grabbed then it will add it
+            if widget is not None:
+                label = QtWidgets.QLabel(w_label)
+
+                if need_label:
+                    layout.addWidget(label)
+                layout.addWidget(widget)
+
+                self.widgets[w_name] = widget
+                if w_type == 'slider':
+                    self.mutable_labels[w_name] = label # tying mutable label to name of widget
+            else:
+                print("Error in Tool Node Wrapper! failed to get widget")
+        
+        container.setLayout(layout)
+        self.set_custom_widget(container)
+
+    def get_value(self):
+        values = {} # this set will contain the values returned, in order of: slider, checkboxes, strings, then combo box
+        # if there is more sensible way to return it, PLEASE modify this.
+
+        for name, widget in self.widgets.items():
+            if isinstance(widget, QtWidgets.QSlider):
+                values[name] = widget.value()
+            elif isinstance(widget, QtWidgets.QCheckBox):
+                values[name] = widget.isChecked()
+            elif isinstance(widget, QtWidgets.QCheckBox):
+                values[name] = widget.toPlainText()
+            elif isinstance(widget, QtWidgets.QComboBox):
+                values[name] = widget.currentText()
+            else:
+                values[name] = None
+
+        print(values)
+
+    def set_value(self, val_dict):
+        for name, val, in val_dict.items():
+            widget = self.widgets.get(name)
+
+            if widget is None:
+                continue
+            if isinstance(widget, QtWidgets.QSlider):
+                widget.setValue(val)
+            elif isinstance(widget, QtWidgets.QCheckBox):
+                widget.setChecked(val)
+            elif isinstance(widget, QtWidgets.QCheckBox):
+                widget.setPlainText(str(val))
+            elif isinstance(widget, QtWidgets.QComboBox):
+                index = widget.findText(str(val))
+                if index >= 0:
+                    widget.setCurrentIndex(index)
+            
+
+    def update_label(self, w_name, val):
+        label = self.mutable_labels.get(w_name)
+
+        if w_name == "thread_slider":
+            label.setText(f'Number of Threads: {val}')
+        elif w_name == "kmers_slider":
+            label.setText(f'Kmer Length: {val}')
+
 class ToolNode(BaseNode):
     __identifier__ = 'bioinformatics_capstone'
     NODE_NAME = 'Tool'
 
     def __init__(self):
         super(ToolNode, self).__init__()
+        self.wrapper = None
+        
+        """ Just as an idea for later. Perhaps we could color code what are 'legal' connections, as in certain colors can only go to other colors. Just a thought"""
+        self.add_input('input', color=(0, 255, 0))
+        self.add_output('output', color=(0, 0, 255))
+    
+    def build_widgets(self, tool):
+        self.wrapper = ToolNodeWrapper(tool, self.view)
+        self.add_custom_widget(self.wrapper)
 
 
 class DataNode(BaseNode):
