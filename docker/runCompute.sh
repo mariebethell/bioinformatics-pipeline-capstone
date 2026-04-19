320a921a133c3345c89d32b951b470b790dc94c0
lopezk38 <lopezk38@gmail.com>
2026-04-19 01:21:20 -0700

    Fixed module path evaluation in container

--- docker/runCompute.sh
+++ docker/runCompute.sh
@@ -2,4 +2,5 @@ export PATH="$PATH:/root/.local/bin"
 alias python='python3.11' python3='python3.11'
 
 . ./venv/bin/activate
+export PYTHONPATH=$PYTHONPATH:. 
 python3 ./network/server/manage.py runserver 0.0.0.0:8000
