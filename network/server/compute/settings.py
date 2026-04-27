6e5b892f604aa330d5cd03346adf56da13142512
lopezk38 <lopezk38@gmail.com>
2026-04-27 00:13:31 -0700

    Disabled unused Django features, cleaned up extra files. This should fix the Django migrations issue

--- network/server/compute/settings.py
+++ network/server/compute/settings.py
@@ -76,12 +76,12 @@ ASGI_APPLICATION = 'network.server.compute.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
