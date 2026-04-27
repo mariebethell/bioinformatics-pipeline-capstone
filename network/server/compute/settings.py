29e33e006bcfccd22f6655277ae6777d7ef92992
lopezk38 <lopezk38@gmail.com>
2026-04-27 00:15:07 -0700

    Actually remove the database setting instead of commenting it out

--- network/server/compute/settings.py
+++ network/server/compute/settings.py
@@ -72,18 +72,6 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'network.server.compute.asgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
