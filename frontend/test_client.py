da4a9519e6e57730caa1a40afa2344510a109234
Marie Bethell <toddbethell56@gmail.com>
2026-04-27 21:56:15 -0700

    Update input node in test_client.py to take reads and tested new pipeline, get pipeline and run pipeline commands

--- frontend/test_client.py
+++ frontend/test_client.py
@@ -12,18 +12,12 @@ from network.client.CommandDispatcher import CommandDispatcher
 from shared.graph import Graph
 from shared.CommandFactory import CommandFactory
 
-class ModelStub:
-    """
-    Model Stub class for storing Graphs instead of AppModel
-    """
-    def __init__(self):
-        graph = Graph()
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname((__file__))))
 
 if __name__ == "__main__":
     user_id_str = "55761d01-6dbb-41a0-b4e2-a501af510697"
     user_id = uuid.UUID(user_id_str)
 
-    # model = ModelStub()
     command_dispatcher = CommandDispatcher(None, user_id)
 
     # Connect to server, which is docker container running locally
@@ -34,18 +28,33 @@ if __name__ == "__main__":
     g = Graph()
 
     n1 = g.create_node("input")
+    n1.outputs = {
+        "reads": [
+            os.path.join(PROJECT_ROOT, "data", "Test01_L001_R1_001.fastq"),
+            os.path.join(PROJECT_ROOT, "data", "Test01_L001_R2_001.fastq"),
+        ]
+    }
+    n1.args = {}  # input nodes don’t have args
+
     n2 = g.create_node("fastqc")
-    n2.args = {'threads': 1, 'format': 'fastq'}
-    n3 = g.create_node("output")
+    n2.args = {} # Use defaults
+    # n3 = g.create_node("output")
 
     g.connect(n1, n2)
-    g.connect(n2, n3)
+    # g.connect(n2, n3)
 
     # Test input uri
     project_directory = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
     input_uri = os.path.join(project_directory, "data", "Test01_L001_R1_001.fastq")
 
-    resp = command_dispatcher.new_pipeline(g, "./shared-data/test1")
+    # Create new pipeline
+    new_pipeline_resp = command_dispatcher.new_pipeline(g, "./shared-data/test1")
+    print(f"new_pipeline_resp:\n{new_pipeline_resp}\n")
 
-    respStr = CommandFactory.serialize_command(resp)
-    print(f"Server returned: {respStr}")
+    # Get created pipeline
+    get_pipeline_resp = command_dispatcher.get_pipeline(new_pipeline_resp.PIPELINE_ID)
+    print(f"get_pipeline_resp:\n{get_pipeline_resp}\n")
+
+    # Run pipeline
+    run_pipeline_resp = command_dispatcher.run_pipeline(new_pipeline_resp.PIPELINE_ID)
+    print(f"run_pipeline_resp:\n{run_pipeline_resp}\n")
